463c37819b9993034c88d639759bafa01de15155
NghiepQuyen <quyen.truongnghiep@gmai.com>
2023-05-03 00:05:27 +0700

    Add project files.

---
+++ ReadPLCSoft/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ReadPLCSoft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgv_main = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_ipAddress = new System.Windows.Forms.TextBox();
+            this.btn_action = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.82587F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.17413F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 115F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 460F));
+            this.tableLayoutPanel1.Controls.Add(this.dgv_main, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tb_ipAddress, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn_action, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.888889F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.11111F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 485);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgv_main
+            // 
+            this.dgv_main.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dgv_main, 4);
+            this.dgv_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_main.Location = new System.Drawing.Point(3, 46);
+            this.dgv_main.Name = "dgv_main";
+            this.dgv_main.Size = new System.Drawing.Size(794, 436);
+            this.dgv_main.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập IP";
+            // 
+            // tb_ipAddress
+            // 
+            this.tb_ipAddress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_ipAddress.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ipAddress.Location = new System.Drawing.Point(81, 11);
+            this.tb_ipAddress.Name = "tb_ipAddress";
+            this.tb_ipAddress.Size = new System.Drawing.Size(140, 20);
+            this.tb_ipAddress.TabIndex = 2;
+            // 
+            // btn_action
+            // 
+            this.btn_action.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_action.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_action.Location = new System.Drawing.Point(227, 10);
+            this.btn_action.Name = "btn_action";
+            this.btn_action.Size = new System.Drawing.Size(109, 23);
+            this.btn_action.TabIndex = 3;
+            this.btn_action.Text = "Tiến hành";
+            this.btn_action.UseVisualStyleBackColor = true;
+            this.btn_action.Click += new System.EventHandler(this.btn_action_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_main)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgv_main;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_ipAddress;
+        private System.Windows.Forms.Button btn_action;
+    }
+}
+
---
+++ ReadPLCSoft/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace ReadPLCSoft
+{
+    public partial class Form1 : Form
+    {
+        System.Data.DataTable dt_data = new System.Data.DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_action_Click(object sender, EventArgs e)
+        {
+            //ushort[] data; // Mảng lưu trữ các giá trị thanh ghi
+            int[] data; // Mảng lưu trữ các giá trị thanh ghi
+            string g_ipAddress = tb_ipAddress.Text.Trim();
+
+            // Kiểm tra địa chỉ IP
+            if (!IPAddress.TryParse(g_ipAddress, out IPAddress ipAddress))
+            {
+                MessageBox.Show("Địa chỉ IP không hợp lệ");
+                return;
+            }
+
+            // Khởi tạo đối tượng ModbusClient
+            ModbusClient modbusClient = new ModbusClient(g_ipAddress, 502);
+
+            try
+            {
+                // Kết nối tới thiết bị
+                modbusClient.Connect();
+
+                // Đọc các thanh ghi từ thiết bị
+                int startingAddress = 0; // Địa chỉ bắt đầu của thanh ghi
+                int quantity = 10; // Số lượng thanh ghi cần đọc
+
+                data = modbusClient.ReadHoldingRegisters(startingAddress, quantity); // Đọc các thanh ghi từ thiết bị và lưu vào mảng 'data'
+
+                // Kiểm tra độ dài của mảng data
+                if (data.Length != quantity)
+                {
+                    MessageBox.Show("Lỗi đọc dữ liệu từ thiết bị");
+                    return;
+                }
+
+                // In ra giá trị của các thanh ghi
+                for (int i = 0; i < quantity; i++)
+                {
+                    //Console.WriteLine("Giá trị thanh ghi " + (startingAddress + i) + ": " + data[i]);
+                    DataRow row = dt_data.NewRow();
+                    row["Col1"] = startingAddress + i;
+                    row["Col2"] = data[i];
+                    dt_data.Rows.Add(row);
+                }
+
+                dt_data.DefaultView.Sort = "Col1";
+                dgv_main.DataSource = dt_data.DefaultView.ToTable();
+            }
+            catch (Exception ex)
+            {
+                if (ex is SocketException)
+                {
+                    MessageBox.Show("Không thể kết nối tới thiết bị");
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            finally
+            {
+                // Đóng kết nối với thiết bị
+                modbusClient.Disconnect();
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt_data.Clear();
+            dt_data.Columns.Add(new DataColumn("Col1", typeof(String)));
+            dt_data.Columns.Add(new DataColumn("Col2", typeof(String)));
+        }
+    }
+}
